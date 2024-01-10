e37da4e1aea057a0ccd6402a6d4a0799b3bc8284
anton.shadaev <anton.shadaev@nordclan.com>
2024-01-10 15:54:03 +0400

    Refactor

--- habr-parser/src/main/java/com/company/habrparser/service/ParserService.java
+++ habr-parser/src/main/java/com/company/habrparser/service/ParserService.java
@@ -9,7 +9,6 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -26,7 +25,7 @@ public class ParserService {
 
     //    @Scheduled(initialDelay = 2000, fixedDelay = 3_600_000)
     @EventListener(ApplicationReadyEvent.class)
-    public void parseAll() {
+    public void findAllVacancies() {
         String title = "java";
         final String url = "https://career.habr.com/vacancies?q=" + title + "&type=all";
         Document doc = null;
@@ -39,19 +38,20 @@ public class ParserService {
         if (doc != null) {
             final Elements sections = doc.getElementsByClass("section-box");
             for (int i = 1; i < 26; i++) {
-                parseWebPage(sections.get(i).getElementsByClass("vacancy-card__title-link").first().absUrl("href"),
+                SendMessageDto message = parseWebPage(
+                        sections.get(i).getElementsByClass("vacancy-card__title-link").first().absUrl("href"),
                         sections.get(i).getElementsByClass("vacancy-card__title").text(),
                         sections.get(i).getElementsByClass("vacancy-card__date").text(),
                         sections.get(i).getElementsByClass("vacancy-card__salary").text(),
                         sections.get(i).getElementsByClass("vacancy-card__company-title").text(),
                         sections.get(i).getElementsByClass("vacancy-card__skills").first().text(),
                         sections.get(i).getElementsByClass("vacancy-card__meta").text());
+                sendMessageToRabbit(message);
             }
         }
-
     }
 
-    private void parseWebPage(String url, String title, String date, String salary, String company, String requirements, String schedule) {
+    private SendMessageDto parseWebPage(String url, String title, String date, String salary, String company, String requirements, String schedule) {
         Document doc = null;
         try {
             doc = Jsoup.connect(url).get();
@@ -60,7 +60,7 @@ public class ParserService {
         }
         if (doc != null) {
             final String description = doc.select("html body.vacancies_show_page div.page-container div.page-container__main div.page-width.page-width--responsive div.content-wrapper div.content-wrapper__main.content-wrapper__main--left section").get(1).text();
-            SendMessageDto vacancy = SendMessageDto.builder()
+            return SendMessageDto.builder()
                     .title(title)
                     .date(date)
                     .salary(salary)
@@ -70,8 +70,8 @@ public class ParserService {
                     .schedule(schedule)
                     .source(url)
                     .build();
-            sendMessageToRabbit(vacancy);
         }
+        return null;
     }
 
     private void sendMessageToRabbit(SendMessageDto sendMessageDto) {
