f9a65a9fadd8c5a12ac1e72a97c9cc3fa9065536
anton.shadaev <anton.shadaev@nordclan.com>
2024-01-22 16:02:48 +0400

    Add db connection

--- aggregator-mvc/src/main/java/com/company/aggregatormvc/controller/AggregatorController.java
+++ aggregator-mvc/src/main/java/com/company/aggregatormvc/controller/AggregatorController.java
@@ -2,13 +2,16 @@ package com.company.aggregatormvc.controller;
 
 import com.company.aggregatormvc.service.AggregatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class AggregatorController {
     private final AggregatorService aggregatorService;
