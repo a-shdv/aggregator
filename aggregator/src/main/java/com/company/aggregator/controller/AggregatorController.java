92eb1fbf1e218647120d4de9a4141bb57c08ce41
anton.shadaev <anton.shadaev@nordclan.com>
2024-01-23 13:49:18 +0400

    Update home.html

--- aggregator/src/main/java/com/company/aggregator/controller/AggregatorController.java
+++ aggregator/src/main/java/com/company/aggregator/controller/AggregatorController.java
@@ -1,6 +1,7 @@
 package com.company.aggregator.controller;
 
 import com.company.aggregator.service.AggregatorService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.PageRequest;
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class AggregatorController {
     private final AggregatorService aggregatorService;
 
@@ -24,7 +30,11 @@ public class AggregatorController {
     public String findAll(@RequestParam(required = false, defaultValue = "0") int page,
                         @RequestParam(required = false, defaultValue = "10") int size,
                         Model model) {
-        model.addAttribute("vacancies", aggregatorService.findAll(PageRequest.of(page, size)));
+        try {
+            model.addAttribute("vacancies", aggregatorService.findAll(PageRequest.of(page, size)).get());
+        } catch (InterruptedException | ExecutionException e) {
+            log.error(e.getMessage());
+        }
         return "home";
     }
 }
